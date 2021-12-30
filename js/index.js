ecdcaea190c15e3df7a171c66f8b89b9ea06cfb3
Homura-san <erickbispo@ufba.br>
2021-12-30 11:56:14 -0300

    sec 1.0

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
+let conf = "branch secundária"
 let a = "v1.0.1"
 let b = "Referências:"
 let c = "https://www.ricardoeletro.com.br/atendimento/entrega"
 let d = "https://www.casasbahia.com.br/CentraldeAtendimento/atendimento.aspx?nid=201404"
 let rep = "https://github.com/Homura-san/projetointegrador"
-console.log(a)
+console.log(conf, a)
 console.log(b)
 console.log(c, d)
 console.log("Repositório: ", rep)
