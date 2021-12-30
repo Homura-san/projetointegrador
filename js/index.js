bcdf5abf58be285b4e399d661642011b8f5ee2ef
Homura-san <erickbispo@ufba.br>
2021-12-30 11:34:22 -0300

    v1.0.1 Console

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let a = "v1.0.1"
+let b = "Referências:"
+let c = "https://www.ricardoeletro.com.br/atendimento/entrega"
+let d = "https://www.casasbahia.com.br/CentraldeAtendimento/atendimento.aspx?nid=201404"
+
+console.log(a)
+console.log(b)
+console.log(c, d)
