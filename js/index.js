63a3f90ee65048cf75755dcf0f5cde32c1b5932c
Homura-san <erickbispo@ufba.br>
2021-12-30 11:45:14 -0300

    1.0

--- js/index.js
+++ js/index.js
@@ -2,7 +2,8 @@ let a = "v1.0.1"
 let b = "Referências:"
 let c = "https://www.ricardoeletro.com.br/atendimento/entrega"
 let d = "https://www.casasbahia.com.br/CentraldeAtendimento/atendimento.aspx?nid=201404"
-
+let rep = "https://github.com/Homura-san/projetointegrador"
 console.log(a)
 console.log(b)
 console.log(c, d)
+console.log("Repositório: ", rep)
